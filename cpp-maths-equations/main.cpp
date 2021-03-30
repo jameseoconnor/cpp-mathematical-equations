c1b989cc2a83a1164421e779b0e97e5a6a733531
James OConnor <jamesedoconnor@gmail.com>
2021-03-30 19:16:28 +0100

    Initial Commit

---
+++ cpp-maths-equations/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-maths-equations
+//
+//  Created by J O'Connor on 30/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
